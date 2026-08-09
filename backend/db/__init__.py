d321e37ad0c6458b1ddfe2f73a6cee4a914c8477
Manabendu-ai <rikugithub@gmail.com>
2026-08-09 15:39:35 +0530

    engine, sessionmaker, base

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -7,3 +7,15 @@ from urllib.parse import quote_plus
 
 DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
 CONNECTION_URL = f"mysql+pymysql://riku:{DB_PASSWORD}@localhost:3306/blogdb"
+
+engine = create_engine(
+    CONNECTION_URL
+)
+
+SessionLocal = sessionmaker(
+    bind = engine,
+    autocommit = False,
+    autoflush=False
+)
+
+Base = declarative_base()
