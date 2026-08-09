83bf5a270ae2faef570edaa7aa12b8c7c2f1fd3a
Manabendu-ai <rikugithub@gmail.com>
2026-08-09 16:51:39 +0530

    added current authenticated user in the user router

--- backend/router/user_router.py
+++ backend/router/user_router.py
@@ -4,6 +4,7 @@ from fastapi.params import Depends
 from sqlalchemy.orm import Session
 from ..db.database import get_db
 from ..repo.user_repo import create_user
+from ..auth.oauth2 import get_current_user
 
 
 router = APIRouter(
@@ -12,5 +13,5 @@ router = APIRouter(
 )
 
 @router.post('/create', response_model=UserResponse)
-def post_user(userReq: UserRequest, db : Session = Depends(get_db)):
+def post_user(userReq: UserRequest, db : Session = Depends(get_db), get_current:UserRequest = Depends(get_current_user)):
     return create_user(userReq, db)
