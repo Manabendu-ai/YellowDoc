c3fe4eb40ac95e7bbe222afc0ff8cedd634d5cdd
Manabendu-ai <rikugithub@gmail.com>
2026-08-09 16:55:10 +0530

    authenticated excel_router

--- backend/router/excel_router.py
+++ backend/router/excel_router.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, UploadFile
 from ..services.file_service import FileService
 from ..services.excel_service import ExcelService
+from ..auth.oauth2 import get_current_user
+from ..dto.user_dto import UserRequest
+from fastapi.params import Depends
 
 router = APIRouter(
     prefix="/excel",
@@ -9,7 +12,7 @@ router = APIRouter(
 
 
 @router.post("/generate")
-async def excel_generator(file : UploadFile, excel_filename: str):
+async def excel_generator(file : UploadFile, excel_filename: str, get_current:UserRequest = Depends(get_current_user)):
 
     file_path = await FileService().save(file)
     excel_file_path = await ExcelService(file_path, excel_filename).convert()
