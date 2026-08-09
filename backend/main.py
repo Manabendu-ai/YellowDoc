561514b9ae65ffae6b23792090560fef54522977
Manabendu-ai <rikugithub@gmail.com>
2026-08-09 16:59:03 +0530

    routed auth router to main app

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from .router.excel_router import router as exr
 from .router.query_router import router as qrr
 from .router.user_router import router as usr
+from .router.auth_router import router as aur
 from .db.database import Base, engine
 
 app = FastAPI(
@@ -20,6 +21,7 @@ Base.metadata.create_all(bind=engine)
 app.include_router(exr)
 app.include_router(qrr)
 app.include_router(usr)
+app.include_router(aur)
 
 @app.get("/")
 def home():
