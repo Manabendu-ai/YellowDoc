5acc5d829dc8e68bfd74a9210f0a33f179121fcf
Manabendu-ai <rikugithub@gmail.com>
2026-08-09 16:55:27 +0530

    authenticated query_router

--- backend/router/query_router.py
+++ backend/router/query_router.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 from ..services.rag_service import RagService
+from ..auth.oauth2 import get_current_user
+from ..dto.user_dto import UserRequest
+from fastapi.params import Depends
 
 router = APIRouter(
     prefix="/query",
@@ -7,6 +10,6 @@ router = APIRouter(
 )
 
 @router.post("")
-async def rag_search(query: str):
+async def rag_search(query: str, get_current:UserRequest = Depends(get_current_user)):
     response = RagService(query).get_response()
     return response
