e38080e1cc1d3c504be47ded8c34584e9a0705c2
Manabendu-ai <rikugithub@gmail.com>
2026-08-25 22:22:19 +0530

    Deployed Database on aiven

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,24 +1,39 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import os
+from sqlalchemy.orm import declarative_base, sessionmaker
 from urllib.parse import quote_plus
+import os
 
-DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
-CONNECTION_URL = f"mysql+pymysql://riku:{DB_PASSWORD}@localhost:3306/yellowdoc"
+load_dotenv()
+
+DB_USER = os.getenv("MYSQL_USER")
+DB_PASSWORD = quote_plus(os.getenv("MYSQL_PASSWORD"))
+DB_HOST = os.getenv("MYSQL_HOST")
+DB_PORT = os.getenv("MYSQL_PORT")
+DB_NAME = "yellowdoc"
+
+CONNECTION_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 engine = create_engine(
-    CONNECTION_URL
+    CONNECTION_URL,
+    connect_args={
+        "ssl": {
+            "ca": os.getenv("SSL_CA_PATH")
+        }
+    }
 )
 
 SessionLocal = sessionmaker(
-    bind = engine,
-    autocommit = False,
+    bind=engine,
+    autocommit=False,
     autoflush=False
 )
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
